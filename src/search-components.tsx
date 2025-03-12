917040dfa8f83de0be2ce57cd275a769848cca2a
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2025-03-12 15:30:47 +0100

    chore: init

---
+++ src/search-components.tsx
@@ -0,0 +1,74 @@
+import { ActionPanel, Action, List, } from "@raycast/api";
+import sections from "./sections";
+import { Filter } from "./types";
+import { useState } from "react";
+
+type State = {
+  filter: Filter;
+  searchText: string;
+};
+
+export default function Command() {
+
+  const [state, setState] = useState<State>({
+    filter: Filter.ALL,
+    searchText: "",
+  });
+
+  const filteredSections = (() => {
+    if (state.filter === Filter.ALL) {
+      return sections ?? [];
+    }
+    
+    return sections?.filter((section) => section.section === state.filter) ?? [];
+  })();
+
+  return (
+    <List
+    searchText={state.searchText}
+    searchBarAccessory={
+      <List.Dropdown
+        tooltip="Select Section"
+        value={state.filter}
+        onChange={(newValue) => setState((previous) => ({ ...previous, filter: newValue as Filter }))}
+      >
+        <List.Dropdown.Item title="All" value={Filter.ALL} />
+        <List.Dropdown.Item title="Containment" value={Filter.CONTAINMENT} />
+        <List.Dropdown.Item title="Navigation" value={Filter.NAVIGATION} />
+        <List.Dropdown.Item title="Form inputs and controls" value={Filter.FORM_INPUTS_CONTROLS} />
+        <List.Dropdown.Item title="Layouts" value={Filter.LAYOUTS} />
+        <List.Dropdown.Item title="Selection" value={Filter.SELECTION} />
+        <List.Dropdown.Item title="Data and display" value={Filter.DATA_DISPLAY} />
+        <List.Dropdown.Item title="Feedback" value={Filter.FEEDBACK} />
+        <List.Dropdown.Item title="Images and icons" value={Filter.IMAGES_ICONS} />
+        <List.Dropdown.Item title="Pickers" value={Filter.PICKERS} />
+        <List.Dropdown.Item title="Providers" value={Filter.PROVIDERS} />
+        <List.Dropdown.Item title="Miscellaneous" value={Filter.MISCELLANEOUS} />
+        
+      </List.Dropdown>
+    }
+    filtering
+    onSearchTextChange={(newValue) => {
+      setState((previous) => ({ ...previous, searchText: newValue }));
+    }}
+    >
+
+      {filteredSections.map((section, index) => (
+        <List.Section key={index} title={section.section}>
+          {section.items.map((item) => (
+            <List.Item
+              key={item.title}
+              title={item.title}
+              actions={
+                <ActionPanel>
+                  <Action.OpenInBrowser url={item.url} />
+                  <Action.CopyToClipboard content={item.url} title="Copy URL" />
+                </ActionPanel>
+              }
+            />
+          ))}
+        </List.Section>
+      ))}
+    </List>
+  );
+}
---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export enum Filter {
+  ALL = "All",
+  CONTAINMENT= "Containment",
+  NAVIGATION= "Navigation",
+  FORM_INPUTS_CONTROLS= "Form Inputs and controls",
+  LAYOUTS= "Layouts",
+  SELECTION= "Selection",
+  DATA_DISPLAY= "Data and display",
+  FEEDBACK= "Feedback",
+  IMAGES_ICONS= "Images and icons",
+  PICKERS= "Pickers",
+  PROVIDERS= "Providers",
+  MISCELLANEOUS= "Miscellaneous",
+};
+
+export enum ComponentCategory {
+  CONTAINMENT = "Containment",
+  NAVIGATION = "Navigation",
+  FORM_INPUTS_CONTROLS = "Form inputs and controls",
+  LAYOUTS = "Layouts",
+  SELECTION = "Selection",
+  DATA_DISPLAY = "Data and display",
+  FEEDBACK = "Feedback",
+  IMAGES_ICONS = "Images and icons",
+  PICKERS = "Pickers",
+  PROVIDERS = "Providers",
+  MISC = "Miscellaneous"
+}
+
+export interface Component {
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface ComponentCategoryGroup {
+  name: ComponentCategory;
+  description?: string;
+  components: Component[];
+}
